8abca05ac3b9e4c4e090ff69fc2034976bcad766
zooid <the.zooid@gmail.com>
2019-07-15 19:55:44 +0200

    commit
    edited the cmd launcher to start pyno without terminal window

--- pyno_run.cmd
+++ pyno_run.cmd
@@ -1,5 +1,5 @@
 @echo off
-Z:\wpy3\python-3.7.1.amd64\python.exe run_pyno.py
+start Z:\wpy3\python-3.7.1.amd64\pythonw.exe run_pyno.py
 rem Z:\wpy2\python-2.7.13.amd64\python.exe run_pyno.py
 rem Z:\ipy2\ipy.exe run_pyno.py
-pause
+#pause
